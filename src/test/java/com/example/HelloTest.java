f71d66958284b861c29ef84df80c5f14b13486cd
Mathieu <48691168+ssfufu@users.noreply.github.com>
2025-05-21 12:51:31 +0200

    Create HelloTest.java

---
+++ src/test/java/com/example/HelloTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloTest {
+
+    @Test
+    public void testHello() {
+        assertTrue(true, "Ce test bidon doit passer");
+    }
+}
